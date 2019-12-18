f485546ab2f4be240f0767ce4918661cba3038dd
Andrey Grabarnick <andreyg@exypnos.co.il>
2019-12-18 12:38:46 +0200

    Add project and todo api files

---
+++ imports/api/projects/projects.js
@@ -0,0 +1,30 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+export const Projects = new Mongo.Collection('projects');
+
+Projects.deny({
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  },
+});
+
+Projects.schema = new SimpleSchema({
+  title: { type: String },
+  description: { type: String },
+  createdAt: { type: Date },
+  dueDate: { type: Date },
+  managerId: { type: String },
+  sharedWithIds: { type: Array },
+  'sharedWithIds.$': { type: String },
+  todos: { type: Array },
+  'todos.$': { type: String },
+});
+
+Projects.attachSchema(Projects.schema);
