59f62696548d71f0d78b946aa8be259b65541999
Andrey Grabarnick <andreyg@exypnos.co.il>
2019-12-20 09:08:34 +0200

    Add some image files

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable react/jsx-fragments */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
 
-function Home() {
+function Home({ user }) {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-9">News</div>
-        <div className="col-3">Log</div>
-      </div>
+    <div className="container-fluid mt-3">
+      {Meteor.userId() ? (
+        <>
+          <div className="row">
+            <div className="col-12">{`Hello, ${user.username}`}</div>
+          </div>
+          <div className="row">
+            <div className="col-9">News</div>
+            <div className="col-3">Log</div>
+          </div>
+        </>
+      ) : (
+        <div className="jumbotron">
+          <img src="/home-banner.png" className="img-fluid" alt="todoer banner" />
+        </div>
+      )}
     </div>
   );
 }
 
-export default Home;
+Home.propTypes = { user: PropTypes.object.isRequired };
+
+export default withTracker(() => ({ user: Meteor.user() }))(Home);
